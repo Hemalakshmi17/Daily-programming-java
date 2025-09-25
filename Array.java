4a7ded582f3d71169e93f815bb0d8d0256b3b2be
Hemalakshmi17 <hemalakshmi131@gmail.com>
2025-09-25 16:03:08 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    String n = sc.nextLine();
+    String [] arr1=n.split;
+    System.out.println(Array.toString(arr1));
+  }
+}
